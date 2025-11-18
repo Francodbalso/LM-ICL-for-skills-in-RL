22263dd6e9e0238b13ff68b7ce8399e4a4a8f410
Francodbalso <francodbalso@gmail.com>
2025-11-18 15:22:05 -0500

    initial

---
+++ agent.py
@@ -0,0 +1,15 @@
+import ollama
+
+class AgentLLM:
+    def __init__(self, model_name, context_size, temperature, max_tokens):
+        super().__init__()
+        self.model_name = model_name
+        self.options = {'num_ctx': context_size, # max number of tokens allowed in context
+                        'temperature': temperature, # sampling temperature
+                        'num_predict': max_tokens # max number of decoded tokens before interrupt
+                        } 
+    
+    def get_action(self, prompt):
+        return ollama.generate(model=self.model_name,
+                        prompt=prompt,
+                        options=self.options)
